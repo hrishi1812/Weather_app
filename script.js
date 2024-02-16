683d40ae2694b2e0fe04530433938a1f11fe65e2
hrishi1812 <rushikeshanarwad@gmail.com>
2024-02-16 12:10:18 +0530

    update

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ async function fetchWhetherData(cityName){
 	let url=`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=a1a95ae641f94b2d2e21105c7b21823e`
 	let response=await fetch(url)
 	const data=await response.json()
-	console.log(data.name)
+	console.log(data)
 }
 function fetchCity(){
 	let cityName=document.getElementById("city_name")
